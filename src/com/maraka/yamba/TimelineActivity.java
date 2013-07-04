e8860a41bb306066643351bc7eaa987a7963ed07
Muhammad Syafrudin <just.udin@yahoo.com>
2013-07-04 15:51:46 +0700

    branch add chapter 10

--- src/com/maraka/yamba/TimelineActivity.java
+++ src/com/maraka/yamba/TimelineActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.format.DateUtils;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
+import android.widget.TextView;
 
 public class TimelineActivity extends Activity {
 	DbHelper dbHelper;
@@ -17,6 +21,7 @@ public class TimelineActivity extends Activity {
 	static final String[] FROM = { DbHelper.C_CREATED_AT, DbHelper.C_USER,
 	DbHelper.C_TEXT }; //
 	static final int[] TO = { R.id.textCreatedAt, R.id.textUser, R.id.textText }; //
+	private static final ViewBinder ViewBinder = null;
 
 
 	@Override
@@ -45,11 +50,28 @@ public class TimelineActivity extends Activity {
 				DbHelper.C_CREATED_AT + " DESC"); //
 		startManagingCursor(cursor); //
 
+		adapter.setViewBinder(ViewBinder); 
+
 		// Create the adapter
 		adapter = new TimelineAdapter(this, cursor); //
 		listTimeline.setAdapter(adapter); //
+		
 
 	}
+	// View binder constant to inject business logic that converts a timestamp to
+	// relative time
+	static final ViewBinder VIEW_BINDER = new ViewBinder() { //
+		public boolean setViewValue(View view, Cursor cursor, int columnIndex) { //
+			if (view.getId() != R.id.textCreatedAt)
+			return false; //
+			// Update the created at text to relative time
+			long timestamp = cursor.getLong(columnIndex); //
+			CharSequence relTime = DateUtils.getRelativeTimeSpanString(view
+			.getContext(), timestamp); //
+			((TextView) view).setText(relTime); //
+			return true; //
+			}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
